23104bba4d8f32788d53c4dd7b05930b9e1b93d0
shivamkr9 <sk9355622@gmail.com>
2025-03-26 12:41:20 +0530

    sdfkjksfkj

--- components/home/subjects.tsx
+++ components/home/subjects.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import { Card, CardContent } from "@/components/ui/card"
